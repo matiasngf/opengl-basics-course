ff7a90698c24978cc795a60fe7e8cae41fa2fbb0
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-12-13 20:03:43 -0300

    add scope to fix gl error

--- src/main.cpp
+++ src/main.cpp
@@ -145,84 +145,87 @@ int main(void)
   std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
   std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 
-  // data for vertex buffer
-  float positions[] = {
-      -0.5f, -0.5f, // 0
-      0.5f, -0.5f,  // 1
-      0.5f, 0.5f,   // 2
-      -0.5f, 0.5f   // 3
-  };
-
-  unsigned int indices[] = {
-      0, 1, 2, // first triangle
-      2, 3, 0  // second triangle
-  };
-
-  // vertex array object
-  unsigned int vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  // send vertex data to the GPU
-  unsigned int buffer;
-
-  int verticesCount = 4;
-  int vertexBufferSize = verticesCount * sizeof(float) * 2; // * 2 because x,y
-
-  // Creates and binds our vertex buffer
-  VertexBuffer vb(positions, vertexBufferSize);
-
-  glEnableVertexAttribArray(0);
-  // this function binds the buffer with the VAO
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-  // create index buffer
-
-  IndexBuffer ib(indices, sizeof(indices));
-
-  unsigned int ibo;
-
-  // create shaders
-  ShaderProgamSource source = ParseShader("src/res/shaders/Basic.shader");
-  unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-
-  // Where in memory
-  int uColorLocation = glGetUniformLocation(shader, "u_Color");
-  ASSERT(uColorLocation != -1);
-
-  float red = 0.0f;
-  float increment = 0.01f;
-
-  while (!glfwWindowShouldClose(window))
   {
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    // enable the shader
-    glUseProgram(shader);
-    // udpate uniform
-    glUniform4f(uColorLocation, red, 1.f, 0.f, 1.f);
+    // data for vertex buffer
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+        0.5f, -0.5f,  // 1
+        0.5f, 0.5f,   // 2
+        -0.5f, 0.5f   // 3
+    };
 
-    // bind current index buffer
-    ib.Bind();
-    // draw elements combines index buffer + vertex buffer
-    GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+    unsigned int indices[] = {
+        0, 1, 2, // first triangle
+        2, 3, 0  // second triangle
+    };
 
-    if (red >= 1.f)
+    // vertex array object
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // send vertex data to the GPU
+    unsigned int buffer;
+
+    int verticesCount = 4;
+    int vertexBufferSize = verticesCount * sizeof(float) * 2; // * 2 because x,y
+
+    // Creates and binds our vertex buffer
+    VertexBuffer vb(positions, vertexBufferSize);
+
+    glEnableVertexAttribArray(0);
+    // this function binds the buffer with the VAO
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    // create index buffer
+
+    IndexBuffer ib(indices, sizeof(indices));
+
+    unsigned int ibo;
+
+    // create shaders
+    ShaderProgamSource source = ParseShader("src/res/shaders/Basic.shader");
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+
+    // Where in memory
+    int uColorLocation = glGetUniformLocation(shader, "u_Color");
+    ASSERT(uColorLocation != -1);
+
+    float red = 0.0f;
+    float increment = 0.01f;
+
+    while (!glfwWindowShouldClose(window))
     {
-      increment = -0.01f;
-    }
-    else if (red <= 0.f)
-    {
-      increment = 0.01f;
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      // enable the shader
+      glUseProgram(shader);
+      // udpate uniform
+      glUniform4f(uColorLocation, red, 1.f, 0.f, 1.f);
+
+      // bind current index buffer
+      ib.Bind();
+      // draw elements combines index buffer + vertex buffer
+      GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+      if (red >= 1.f)
+      {
+        increment = -0.01f;
+      }
+      else if (red <= 0.f)
+      {
+        increment = 0.01f;
+      }
+
+      red += increment;
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
     }
 
-    red += increment;
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    glDeleteProgram(shader);
   }
-
-  glDeleteProgram(shader);
 
   glfwTerminate();
   return 0;
